a72e11e96724b4514aa36140ff27bf3766068655
Fadilah Hasan <fadilah.hasan@metraplasa.co.id>
2020-08-25 00:19:12 +0700

    initial

---
+++ Bareksa Test/Bareksa Test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bareksa Test
+//
+//  Created by Fadilah Hasan on 25/08/20.
+//  Copyright © 2020 KodingKita. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
